b68be55751d23ee2bef828a029299a6dcfecf1bc
hariv1 <hariv@rose-hulman.edu>
2017-12-05 01:13:26 -0500

    DONE

--- src/m4_more_summing_and_counting.py
+++ src/m4_more_summing_and_counting.py
@@ -306,13 +306,21 @@ def count_cosines_from(m, n, x):
       -- count_cosines_from(4, 8, -0.5)  returns  4
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     # IMPORTANT: As in previous problems in this session,
     #   you must NOT use the 2 or 3-parameter versions
     #   of the RANGE expression, if you happen to know them.
     # ------------------------------------------------------------------
+
+    total = 0
+
+    for k in range(n-m+1):
+        if math.cos(m+k)> x:
+            total = total +1
+
+    return total
 
 
 def run_test_sum_unit_fractions_from():
